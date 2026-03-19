fdba58fb50ae98979794df0df13608b50c72c5fc
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-19 16:59:42 +0100

    initial commit: - react suspense course module

---
+++ 04-react-suspense/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Feature01 from "./features/01/Feature01";
+
+function App() {
+  return (
+    <>
+      <h1>React Suspense</h1>
+      <Feature01 />
+    </>
+  );
+}
+
+export default App;
---
+++ 04-react-suspense/src/features/01/Feature01.tsx
@@ -0,0 +1,4 @@
+function Feature01() {
+  return <div>Feature01</div>;
+}
+export default Feature01;
