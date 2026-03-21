6677e4eb6684efb921212704b4cf8cf49adb3ace
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-21 11:28:26 +0100

    prevent footer re-render when count changes

--- 05-react-performance/src/features/01/Feature01.tsx
+++ 05-react-performance/src/features/01/Feature01.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
+function Footer() {
+  return <footer>I'm the footer</footer>;
+}
+
+const footer = <Footer />;
+
 function Feature01() {
-  return <div>Feature01</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>
+        <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+        <br />
+        <br />
+      </div>
+      {footer}
+    </>
+  );
 }
 export default Feature01;
