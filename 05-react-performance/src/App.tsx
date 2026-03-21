cd0913307b28855f256d018b7db646bb5a2cf064
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-21 11:08:33 +0100

    React performance initial commit

---
+++ 05-react-performance/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Feature01 from "./features/01/Feature01";
+
+function App() {
+  return (
+    <>
+      <h1>React Performance</h1>
+      <Feature01 />
+    </>
+  );
+}
+
+export default App;
